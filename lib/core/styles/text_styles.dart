08357956eaa3546f67dd83c9f68fca27b4fd129a
Mohamed Ali <ma8510007@gmail.com>
2025-05-04 18:24:55 +0300

    - added app colors - added app text styles

---
+++ lib/core/styles/text_styles.dart
@@ -0,0 +1,43 @@
+import 'dart:ui';
+
+import 'package:bmi_app/core/theme/colors_manger.dart';
+
+class TextStyles {
+  static TextStyle font20W400 = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.w400,
+    color: ColorsManger.textColor,
+  );
+
+  static TextStyle font20W600White = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.w600,
+    color: ColorsManger.whiteColor,
+  );
+  static TextStyle font20W300 = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.w300,
+    color: ColorsManger.textColor,
+  );
+  static TextStyle font16W500 = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w500,
+    color: ColorsManger.textColor,
+  );
+
+  static TextStyle font40W700White = TextStyle(
+    fontSize: 40,
+    fontWeight: FontWeight.w700,
+    color: ColorsManger.whiteColor,
+  );
+  static TextStyle font32W600White = TextStyle(
+    fontSize: 32,
+    fontWeight: FontWeight.w600,
+    color: ColorsManger.whiteColor,
+  );
+  static TextStyle font20W700Green = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.w700,
+    color: ColorsManger.greenColor,
+  );
+}
---
+++ lib/core/theme/colors_manger.dart
@@ -0,0 +1,10 @@
+import 'dart:ui';
+
+class ColorsManger {
+  static const Color scaffoldBackgroundColor = Color(0xFF1C2135);
+  static const Color selcetedContainerColor = Color(0xff333244);
+  static const Color unSelcetedContainerColor = Color(0xff24263B);
+  static const Color textColor = Color(0xFF8B8C9E);
+  static const Color whiteColor = Color(0xFFFFFFFF);
+  static const Color greenColor = Color(0xff21BF73);
+}
